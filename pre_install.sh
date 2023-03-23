e3eba8816a63442e63e73ef40f8502e73fbfeed3
ChinYikMing <yikming2222@gmail.com>
2023-03-23 23:03:27 +0800

    initial commit

---
+++ pre_install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+apt-get -y update
+apt-get -y install openssh-server
+apt-get -y install apache2
+apt-get -y install vim
+
+sshd_config_file="/etc/ssh/sshd_config"
+echo "Port 22" >> $sshd_config_file
+echo "AddressFamily any" >> $sshd_config_file
+echo "ListenAddress 0.0.0.0" >> $sshd_config_file
+echo "ListenAddress ::" >> $sshd_config_file
+echo "PermitRootLogin yes" >> $sshd_config_file
+
+service ssh start
+service apache2 start
+
+echo "root:root" | chpasswd
+
+tail -f /dev/null
